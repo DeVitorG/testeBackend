72af126097c161161deb0a3360a89f67603725ba
Vitor Gabriel <dev.vitor.gabriel@gmail.com>
2022-10-19 20:15:33 -0300

    feat: ajustes;

--- application/controllers/ApiNoticia.php
+++ application/controllers/ApiNoticia.php
@@ -3,21 +3,95 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class ApiNoticia extends CI_Controller {
-    
-    public function create(){
 
+    //Dentro do construtor foi definido uma rota padrão para conexão com o banco.
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->load->model('ApiNoticia_model', 'ApiNoticia');
     }
 
-    public function edit(){
-        
-    }
 
-    public function lista(){
-        
-    }
+    // Criação da função de criação da noticia.
+    public function criar()
+    {   
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Headers: content-type");
+        $json_post = json_decode(file_get_contents("php://input"));
 
-    public function delete(){
+        $obj = new stdClass();
+        if($json_post->noticia <> null)
+        {   
+            $cod = $this->ApiNoticia->criarNoticia($json_post);
+            if($cod > 0)
+            {
+                $array['Mensagem'] = 'A notícia foi cadastrada com sucesso!'; 
+                $obj->status = 200;
+                $obj->data = $array;
+            }else{
+            $array['Mensagem'] = 'A notícia não foi cadastrada com sucesso!'; 
+            $obj->status = 500;
+            $obj->data = $array;
+            } 
+        }   
+        else
+        {
+            $array['Mensagem'] = 'Não foram definidos os parâmetros necessários!'; 
+            $obj->status = 404;
+            $obj->data = $array;
+        }
+        echo json_encode($obj);
         
+
+    }
+    // Criação da função de edição da noticia já cadastrada.
+    public function editar()
+    {
+        $obj = new stdClass();
+        if(count($_GET) > 0)
+        {
+            
+        }
+        else
+        {
+            $array['Mensagem'] = 'Não foram definidos os parâmetros necessários!'; 
+            $obj->status = 404;
+            $obj->data = $array;
+        }
+        echo json_encode($obj);
+    }
+    // Criação da função da lista de notícias já cadastradas.
+    public function listar()
+    {
+        $obj = new stdClass();
+        if(count($_GET) > 0)
+        {
+            
+        }
+        else
+        {
+            $array['Mensagem'] = 'Não foram definidos os parâmetros necessários!'; 
+            $obj->status = 404;
+            $obj->data = $array;
+        }
+        echo json_encode($obj);
+    }
+    // Criação da função de delete da notícia já cadastrada.
+    public function deletar()
+    {
+        $obj = new stdClass();
+        if(count($_GET) > 0)
+        {
+            
+        }
+        else
+        {
+            $array['Mensagem'] = 'Não foram definidos os parâmetros necessários!'; 
+            $obj->status = 404;
+            $obj->data = $array;
+        }
+        echo json_encode($obj);
     }
 
 
--- application/models/ApiNoticia_model.php
+++ application/models/ApiNoticia_model.php
@@ -8,5 +8,25 @@ class ApiNoticia_model extends CI_Model {
         parent::__construct();
     }
 
+    public function criarNoticia($argumentos)
+    {
+        try 
+        {
+            $this->db->set('des_noticia', $argumentos->noticia);
+            $this->db->set('dta_cadastro', date('Y-m-d H:i:s'));
+            $this->db->insert('noticias');
+
+            $db_error = $this->db->error();
+
+            if (!empty($db_error['message'])) {
+                throw new Exception('Database error! Error Code [' . $db_error['code'] . '] Error: ' . $db_error['message']);
+                return FALSE;
+            }
+            return $this->db->insert_id();
+        } catch (Exception $e) {
+            return -1;
+        }
+        
+    }
 
 }
