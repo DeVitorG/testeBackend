d6c1a5855adeba9cb41c6843047cc3291deb035a
Vitor Gabriel <dev.vitor.gabriel@gmail.com>
2022-10-19 17:03:08 -0300

    first commit

---
+++ application/controllers/ApiNoticia.php
@@ -0,0 +1,26 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class ApiNoticia extends CI_Controller {
+    
+    public function create(){
+
+    }
+
+    public function edit(){
+        
+    }
+
+    public function lista(){
+        
+    }
+
+    public function delete(){
+        
+    }
+
+
+}
+
+?>
